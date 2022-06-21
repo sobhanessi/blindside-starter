17f955c626499ebc724c4fd4782e94ad22e2b98e
sobhanessi <sobhanessi@gmail.com>
2022-06-21 16:40:28 +0300

    going good

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,20 +1,57 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AutoComplete from "@mui/material/Autocomplete";
 import {
+  Button,
   Card,
   CardContent,
   CardMedia,
   Container,
   Grid,
+  Switch,
   TextField,
   Typography,
 } from "@mui/material";
 import videos from "./videos";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 function HomePage() {
-  const [video, setVideo] = React.useState("");
-  //   console.log(videos.map((video) => video.title === title));
-  console.log(typeof video.src);
+  const [video, setVideo] = React.useState([]);
+  const [related, setRelated] = React.useState([]);
+  const [comments, setComments] = React.useState([]);
+  const [showComment, setShowComment] = React.useState(true);
+
+  const validationSchema = Yup.object({
+    comment: Yup.string(),
+  });
+
+  console.log(comments);
+
+  const onChange = () => {
+    let filtered = videos.filter(
+      (v) => JSON.stringify(v.categories) === JSON.stringify(video.categories)
+    );
+    setRelated(filtered);
+    setComments(video.comments);
+  };
+  useEffect(() => {
+    onChange();
+  }, [video]);
+
+  const formik = useFormik({
+    initialValues: {
+      comment: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => setNewComments(values.comment),
+  });
+
+  const setNewComments = (comment) => {
+    return comments.length
+      ? setComments([...comments, comment])
+      : setComments([comment]);
+  };
+
   return (
     <>
       <nav></nav>
@@ -52,22 +89,76 @@ function HomePage() {
               )}
             />
           </Grid>
-          <Card>
-            <CardContent>
-              <Typography>Hello world</Typography>
-            </CardContent>
-            {/* {video.length && ( */}
-            <CardMedia
-              component="video"
-              image={video.src}
-              autoPlay={true}
-              controls
-            />
-            {/* <video controls>
-              <source src={} type="video/mp4" />
-            </video> */}
-            {/* )} */}
-          </Card>
+          {Object.keys(video).length > 0 ? (
+            <Card>
+              <CardContent>
+                <Typography>{video.title}</Typography>
+              </CardContent>
+              <CardMedia component="video" image={video.src} controls />
+              <CardContent>
+                <Typography gutterBottom>
+                  You can leave your comment by writing in the box below or you
+                  can toggle it off if you wish to not to see the comments!{" "}
+                  <Switch
+                    checked={showComment}
+                    onChange={() => setShowComment(!showComment)}
+                  />
+                </Typography>
+
+                <form onSubmit={formik.handleSubmit}>
+                  {video && showComment && (
+                    <>
+                      <TextField
+                        label="Leave your comment"
+                        variant="outlined"
+                        sx={{ mb: 2 }}
+                        onChange={formik.handleChange}
+                        name="comment"
+                      />
+                      <Button type="submit">Submit</Button>
+                      {video && comments ? (
+                        comments.map((cm) => <Typography>{cm}</Typography>)
+                      ) : (
+                        <Typography>
+                          be the first one to write a comment.
+                        </Typography>
+                      )}
+                    </>
+                  )}
+                </form>
+              </CardContent>
+            </Card>
+          ) : (
+            <Typography>search for a video</Typography>
+          )}
+
+          <Grid
+            container
+            display="flex"
+            sx={{
+              mt: 3,
+              mb: 3,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            {related ? (
+              related.map((r) => (
+                <Grid item sm={5} sx={{ mr: 2.2 }}>
+                  <span onClick={() => setVideo(r)}>
+                    <Card>
+                      <CardMedia component="video" image={r.src} />
+                      <CardContent>
+                        <Typography>{r.title}</Typography>
+                      </CardContent>
+                    </Card>
+                  </span>
+                </Grid>
+              ))
+            ) : (
+              <Typography>Couldn't find any related video</Typography>
+            )}
+          </Grid>
         </Container>
 
         <div>here I must show the searched videos.</div>
--- src/videos.js
+++ src/videos.js
@@ -5,31 +5,37 @@ const videos = [
     title: "Welcome",
     src: WelcomeReact,
     comments: [""],
+    categories: ["React"],
   },
   {
     title: "Learning React: Advanced",
     src: "",
     comments: [""],
+    categories: ["React"],
   },
   {
     title: "Learning React Native: Fundamentals",
     src: "",
     comments: [""],
+    categories: ["React Native", "React"],
   },
   {
     title: "Learning React Native: Advanced",
     src: "",
     comments: [""],
+    categories: ["React Native", "React"],
   },
   {
     title: "Learning Redux: Fundamentals",
     src: "",
     comments: [""],
+    categories: ["Redux"],
   },
   {
     title: "Learning Redux: Advanced",
     src: "",
     comments: [""],
+    categories: ["Redux"],
   },
 ];
 
